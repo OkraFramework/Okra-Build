b9447d88f53a3ba07baede556cf5a7d3b4ad95a8
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2016-05-22 16:20:30 +0100

    Add Sake based build scripts

---
+++ build.ps1
@@ -0,0 +1,15 @@
+# Import Modules
+
+Import-Module -Name ".\.build\scripts\Invoke-NuGet.psm1"
+
+# Install Sake
+
+If (!(Test-Path ".\packages\Sake.0.2.2"))
+{
+    Install-NuGet
+    Install-NuGetPackage Sake 0.2.2 .\packages
+}
+
+# Run build steps
+
+.\packages\Sake.0.2.2\tools\Sake.exe -I .build\shade -f .build\shade\makefile.shade @args
